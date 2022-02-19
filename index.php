7c0bd40a5baddc1a63a516006b9bafa59ca5823c
joelclementes <joelclementes.dev@gmail.com>
2022-02-18 21:13:49 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Estudio de dones específicos</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center teal-text text-lighten-4">Estudio de dones específicos</h1>
+        <div class="row center">
+          <h6 class="header col s12 light">Estudio de algunos dones mencionados en 1Corintios 12:7-10, 28-31; 14:1-40 como <em>dones extraordinarios.</em></h6>
+        </div>
+        <!-- <br><br> -->
+
+      </div>
+    </div>
+    <div class="parallax"><img src="fondo1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">accessibility</i></h2>
+            <h5 class="center">1a Corintios 12:7-10</h5>
+
+            <p class="light">
+              Pero a cada uno le es dada la manifestación del Espíritu para provecho. Porque a éste es dada por el Espíritu palabra de sabiduría; a otro, palabra de ciencia según el mismo Espíritu; a otro, fe por el mismo Espíritu; y a otro, dones de sanidades por el mismo Espíritu. A otro, el hacer milagros; a otro, profecía; a otro, discernimiento de espíritus; a otro, diversos géneros de lenguas; y a otro, interpretación de lenguas.
+            </p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">1a Corintios 12:28-31</h5>
+
+            <p class="light">
+              Y a unos puso Dios en la iglesia, primeramente apóstoles, luego profetas, lo tercero maestros, luego los que hacen milagros, después los que sanan, los que ayudan, los que administran, los que tienen don de lenguas. ¿Son todos apóstoles? ¿son todos profetas? ¿todos maestros? ¿hacen todos milagros? ¿Tienen todos dones de sanidad? ¿hablan todos lenguas? ¿interpretan todos? Procurad, pues, los dones mejores. Mas yo os muestro un camino aun más excelente.
+            </p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">directions_run</i></h2>
+            <h5 class="center">1a Corintios 14:1</h5>
+
+            <p class="light">Seguid el amor; y procurad los dones espirituales</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h3 class="header col s12 light">Sirviendo en el reino de Dios.</h3>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="fondo3.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+      <div class="row">
+        <h5>Aprende acerca de los dones aquí descritos:</h5>
+      </div>
+      <div class="row">
+      <ul class="collapsible">
+        <li>
+            <?php include("listaDeDones/donDeAdministracion.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeArte.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeDar.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeDiscernimiento.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeEnseñanza.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeEvangelizar.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeExhortacion.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeFe.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeHospitalidad.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeIntercesion.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeLiderazgo.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeMisericordia.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDePastoral.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeProfecia.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/donDeServicio.html"); ?>
+        </li>
+        <li>
+            <?php include("listaDeDones/descubreTuDon.html"); ?>
+        </li>
+      </ul>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h6 class="header col s12 light">
+            ¡Sirvan al Señor con alegría! ¡Vengan a su presencia con regocijo! Reconozcan que el Señor es Dios; él nos hizo, y de él somos. Somos su pueblo. ¡Somos las ovejas de su prado!
+          </h6>
+          <h5 class="header col s12 light">
+            Salmos 100:1-2
+          </h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="fondo2.jpg" alt="Unsplashed background img 3"></div>
+  </div>
+
+  <footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+
+      <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Para reflexionar</h4>
+          <p>
+            <h6>¿Cómo ha usado Dios mi don o mis dones para bendecir las vidas de los demás?</h6>
+          </p>
+          <p>
+            <h6>¿Qué han dicho otras personas acerca de mí que han confirmado que tengo uno o más dones?</h6>
+          </p>
+          <p>
+            <h6>¿Qué problemas he encontrado al usar uno o más dones?</h6>
+          </p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="footer-copyright">
+      <div class="container">
+      Para más estudios bíblicos y de discipulado <a target = "_blank" class="brown-text text-lighten-3" href="https://www.youtube.com/channel/UCNNRkFTfZY1hkquB_GMgPjw">clic aquí</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
